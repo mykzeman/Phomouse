834111cbc4562c7c1a0266b8cd9131e83b84810f
mykzeman <mykalmayne@gmail.com>
2026-04-11 18:42:31 +1200

    Implement Bluetooth HID device connection logic
    - Add BLUETOOTH and BLUETOOTH_CONNECT permissions to AndroidManifest.xml.
    - Add a "Connect" button to the main layout.
    - Implement Bluetooth HID_DEVICE profile proxy connection in MainActivity.
    - Update deployment target configurations.

--- app/src/main/java/osa/phomouse/MainActivity.kt
+++ app/src/main/java/osa/phomouse/MainActivity.kt
@@ -1,20 +1,36 @@
 package osa.phomouse
 
+import android.bluetooth.BluetoothHidDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        
+        val btnConnect = findViewById<Button>(R.id.btnConnect)
+
+        btnConnect.setOnClickListener {
+            val bluetoothManager = getSystemService(BluetoothManager::class.java)
+            val bluetoothAdapter = bluetoothManager?.adapter
+
+            bluetoothAdapter?.getProfileProxy(
+                this,
+                object : BluetoothProfile.ServiceListener {
+                    override fun onServiceConnected(profile: Int, proxy: BluetoothProfile) {
+                        if (profile == BluetoothProfile.HID_DEVICE) {
+                            val hidDevice = proxy as BluetoothHidDevice
+                            // Use hidDevice here
+                        }
+                    }
+                    override fun onServiceDisconnected(profile: Int) { }
+                },
+                BluetoothProfile.HID_DEVICE
+            )
         }
     }
 }
