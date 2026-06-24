ea932d060a1c6bf47818843e672fc7edff4eb87c
mykzeman <mykalmayne@gmail.com>
2026-06-24 13:41:30 +1200

    Refactor build.gradle.kts: remove unused import and viewBinding configuration, simplify exception handling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-import java.net.URL
 import java.net.URI
 
 plugins {
@@ -59,9 +58,6 @@ android {
     viewBinding = true
   }
   testOptions { unitTests { isIncludeAndroidResources = true } }
-    viewBinding {
-        isEnabled = true
-    }
     dependenciesInfo {
         includeInBundle = true
         includeInApk = true
@@ -120,7 +116,7 @@ tasks.register("downloadFonts") {
                                 println("Successfully copied system font fallback to $name")
                                 copied = true
                                 break
-                            } catch (ex: Exception) {
+                            } catch (_: Exception) {
                                 // ignore
                             }
                         }
